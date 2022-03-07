ec4f5d355fa0785defe36b3ab66078a66e240fd0
{shreya-gupta} <shreya.gupta@vodafone.com>
2022-03-07 14:18:27 +0000

    added styling to css to make it more dynamic

--- src/App.js
+++ src/App.js
@@ -199,7 +199,6 @@ export default function Example() {
             {/* Replace with your content */}
             <div className="px-4 py-6 sm:px-0">
               <TodoList />
-              <div className="border-4 border-dashed border-gray-200 rounded-lg h-96" />
             </div>
             {/* /End replace */}
           </div>
