33fe2aaf6bbc63a1c63dfb3e442010e0985a5360
{shreya-gupta} <shreya.gupta@vodafone.com>
2022-03-07 13:33:28 +0000

    added format to list so it saves and displays it

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,13 +1,38 @@
 import React, {useState} from 'react'
+import Todo from './Todo'
 import TodoForm from './TodoForm'
 
 
 function TodoList() {
   const [todos, SetTodos] = useState([])
+
+  const addTodo = todo => {
+    if(!todo.text || /^\s*$/.test(todo.text)) {
+      return
+    }
+
+    const newTodos = [todo, ...todos];
+
+    SetTodos(newTodos);
+  };
+
+const completeTodo = id => {
+  let updatedTodos = todos.map(todo => {
+    if (todo.id === id){
+      todo.isComplete = !todo.isComplete
+    }
+    return todo
+  })
+  SetTodos(updatedTodos);
+}
+
   return (
     <div>
       <h1>What Cyber Security measures would you like to add?</h1>
-      <TodoForm/>
+      <TodoForm onSubmit={addTodo}/>
+      <Todo
+      todos={todos}
+      completeTodo={completeTodo} />
     </div>
   )
 }
