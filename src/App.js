e991ec329de0d4973ffc95d6cdb3671aedc77954
{shreya-gupta} <shreya.gupta@vodafone.com>
2022-03-07 16:52:39 +0000

    added routing functionality to allow notifications to direct you to a different page

--- src/App.js
+++ src/App.js
@@ -192,21 +192,13 @@ export default function Example() {
             </>
           )}
         </Disclosure>
-
-        <header className="bg-white shadow">
-          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
-          </div>
-        </header>
         <main>
           <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             {/* Replace with your content */}
             <div className="px-4 py-6 sm:px-0">
               <Routes>
-              <Route path="/" element={<TodoList></TodoList>}>
-                <Route path="notifications" element={<Notifications/>}/>
-                
-                </Route>
+              <Route path="/" element={<TodoList></TodoList>}/>
+                <Route path="notifications/:id" element={<Notifications/>}/>
                 </Routes>
              
             </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,6 +7,7 @@ import { Routes, Route, Link } from "react-router-dom";
 
 
 
+
 function Todo({todos, completeTodo, removeTodo}) {
     const [edit, setEdit] = useState({id: null, value: ''})
 
@@ -14,7 +15,14 @@ function Todo({todos, completeTodo, removeTodo}) {
 
 
 
-  return todos.map((todo,index) => (<div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
+  return(
+      <div>
+           <header className="bg-white shadow">
+          <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <h1 className="text-3xl font-bold text-gray-900">Dashboard</h1>
+          </div>
+        </header>
+        {todos.map((todo,index) => (<div className={todo.isComplete ? 'todo-row complete' : 'todo-row'} key={index}>
 
       <div key={todo.id} onClick={() => completeTodo(todo.id)}>
           {todo.text}
@@ -25,14 +33,16 @@ function Todo({todos, completeTodo, removeTodo}) {
           <RiCloseCircleLine
           onClick={() => removeTodo(todo.id)} className = 'delete-icon'/>
 
-          <GrNotification className='notification-icon'/>
-
           </div>
       <Link to="notifications"> Notifications</Link> 
       </div> 
-      ))
+      ))}
       
+      </div>
+  ) 
   
 }
+
+
 
 export default Todo
